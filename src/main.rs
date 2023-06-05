f25c04ddb0e19bd7a9dae737f84879b29e1f45b8
CarlosLaraFP <info@carloslaraai.com>
2023-06-05 11:40:51 -0700

    Binary search infinite loop

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use std::io;
     message caused by an I/O error being returned from within our program will be displayed user-friendly.
  */
 use rand::Rng; // The Rng trait defines methods that random number generators implement
+use std::cmp::Ordering; // enum with the variants Less, Greater, and Equal. These are the three outcomes that are possible when you compare two values.
 
 
 fn main() -> anyhow::Result<()> {
@@ -22,28 +23,31 @@ fn main() -> anyhow::Result<()> {
     // Q# style syntax for range where = indicates inclusive
     // the rand::thread_rng function that gives us the particular random number generator:
     // one that is local to the current thread of execution and is seeded by the operating system
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // string type provided by the standard library that is a growable, UTF-8 encoded bit of text
-    let mut guess = String::new();
+        // string type provided by the standard library that is a growable, UTF-8 encoded bit of text
+        let mut guess = String::new();
 
-    // The stdin function returns an instance of std::io::Stdin, which is a type that represents a handle to the standard input for the terminal
-    // Result is an enum, which is a type that can be in one of multiple possible states. We call each possible state a variant (Ok and Err).
-    // Returns the number of bytes in the user’s input
-    io::stdin().read_line(&mut guess)?;
+        // The stdin function returns an instance of std::io::Stdin, which is a type that represents a handle to the standard input for the terminal
+        // Result is an enum, which is a type that can be in one of multiple possible states. We call each possible state a variant (Ok and Err).
+        // Returns the number of bytes in the user’s input
+        io::stdin().read_line(&mut guess)?;
 
-    // shadowing and trimming the newline character \n that gets appended when the user hits enter after typing their input
-    // Without anyhow, the trait `From<ParseIntError>` is not implemented for `std::io::Error`
-    let guess = guess.trim().parse::<i32>()?;
+        // Shadowing and trimming the newline character \n that gets appended when the user hits enter after typing their input
+        // Without anyhow, the trait `From<ParseIntError>` is not implemented for `std::io::Error`
+        let guess = guess.trim().parse::<u32>()?;
 
-    if secret_number == guess {
-        println!("You guessed {secret_number} correctly!");
-    }
-    //else if secret_number > guess { }
-    else {
-        println!("You guessed: {guess}, but the secret number was {secret_number}");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small. Try a bigger number!"),
+            Ordering::Greater => println!("Too big. Try a small number!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 
     Ok(())
