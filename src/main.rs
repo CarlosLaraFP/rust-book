9947baf772a3389c0e71d083d3e94b680d6243f5
CarlosLaraFP <info@carloslaraai.com>
2023-06-05 11:49:22 -0700

    Improved error handling as project completion

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
     scope of every program. This set is called the prelude.
  */
 use std::io;
-// use anyhow::anyhow; // Only use anyhow! macro if no default implementation is already provided by the crate
+use anyhow::anyhow; // Only use anyhow! macro if no default implementation is already provided by the crate
 /*
     The logic behind the anyhow crate is that it provides its own error type. This type has
     pretty-printing properties and can easily be converted from other errors, like std::io::Error.
@@ -38,14 +38,20 @@ fn main() -> anyhow::Result<()> {
 
         // Shadowing and trimming the newline character \n that gets appended when the user hits enter after typing their input
         // Without anyhow, the trait `From<ParseIntError>` is not implemented for `std::io::Error`
-        let guess = guess.trim().parse::<u32>()?;
+        let guess = match guess.trim().parse::<u32>() {
+            Ok(num) => num,
+            Err(error) => {
+                println!("{error}");
+                continue
+            }
+        };
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small. Try a bigger number!"),
             Ordering::Greater => println!("Too big. Try a small number!"),
             Ordering::Equal => {
                 println!("You win!");
-                break;
+                break
             }
         }
     }
