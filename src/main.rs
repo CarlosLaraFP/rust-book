67162af2417919f9ecb01f454401d651c902b2eb
CarlosLaraFP <info@carloslaraai.com>
2023-06-05 07:40:57 -0700

    Rust guessing game algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)?;
+
+    println!("You guessed: {guess}");
+
+    Ok(())
 }
