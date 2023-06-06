9b83f394fd3e7ad2c4c8f6e6f4e1307f9ddfefde
CarlosLaraFP <info@carloslaraai.com>
2023-06-06 10:21:46 -0700

    Nested loops with label

--- src/main.rs
+++ src/main.rs
@@ -78,4 +78,27 @@ fn main() {
     };
 
     println!("The result is {result}");
+
+    let mut count = 0;
+
+    // loop label for nested loops
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+
+    println!("End count = {count}");
 }
